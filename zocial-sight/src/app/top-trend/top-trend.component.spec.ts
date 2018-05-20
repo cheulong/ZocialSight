144b5151aa01d070efc91c9d09d8d617382e6037
cheulong <searcheulong@gmail.com>
2018-05-20 08:06:02 +0700

    add top-trend component

---
+++ zocial-sight/src/app/top-trend/top-trend.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TopTrendComponent } from './top-trend.component';
+
+describe('TopTrendComponent', () => {
+  let component: TopTrendComponent;
+  let fixture: ComponentFixture<TopTrendComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TopTrendComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TopTrendComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ zocial-sight/src/app/top-trend/top-trend.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-trend',
+  templateUrl: './top-trend.component.html',
+  styleUrls: ['./top-trend.component.scss']
+})
+export class TopTrendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
