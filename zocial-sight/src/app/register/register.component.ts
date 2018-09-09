1b35c877edf981e0409c738755c8e9576115fee9
unknown <alienware16x@gmail.com>
2018-09-09 16:02:03 +0700

    Test

--- zocial-sight/src/app/register/register.component.ts
+++ zocial-sight/src/app/register/register.component.ts
@@ -1,28 +1,32 @@
-import { Component } from "@angular/core";
-import { AuthService } from "./../services/auth.service";
-import { User } from "./../models/user";
-import { Router } from "@angular/router";
+import {Component} from "@angular/core";
+import {AuthService} from "./../services/auth.service";
+import {User} from "./../models/user";
+import {Router} from "@angular/router";
+
 @Component({
   selector: "register",
   templateUrl: "./register.component.html",
   styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent {
+  username = "";
   user: User = new User();
   foods = [
-    { value: "1", viewValue: "What is your favorite food?" },
-    { value: "2", viewValue: "What is your favorite sport?" }
+    {value: "1", viewValue: "What is your favorite food?"},
+    {value: "2", viewValue: "What is your favorite sport?"}
   ];
   roles = [
-    { value: "1", viewValue: "Staff" },
-    { value: "2", viewValue: "Product Owner" }
+    {value: "1", viewValue: "Staff"},
+    {value: "2", viewValue: "Product Owner"}
   ];
   selected = "1";
   selectedRole = "1";
+
   constructor(private router: Router, private auth: AuthService) {
     this.user.statue = " Product Owner ";
     this.user.question = " What is your favorite food? ";
   }
+
   onRegister(): void {
     // console.log('user',this.user);
     if (confirm("Are you sure to add new user?")) {
@@ -39,11 +43,13 @@ export class RegisterComponent {
       console.log("Cancel");
     }
   }
+
   getGender(e) {
     console.log(e);
 
     this.user.question = e;
   }
+
   getRole(e) {
     console.log(e);
 
